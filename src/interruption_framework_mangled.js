69d6f8a3e642718168fe04cd338103a903ea3568
delfineonx <delfineonx@gmail.com>
2025-12-04 14:24:34 +0200

    Improve performance

--- src/interruption_framework_mangled.js
+++ src/interruption_framework_mangled.js
@@ -8,8 +8,8 @@
     handler:()=>{},
     args:[],
     delay:0,
-    limit:2,
-    phase:1,
+    limit:1,
+    phase:400000,
     cache:null,
     defaultPhase:400000,
     wasInterrupted:!1,
@@ -19,7 +19,7 @@
   E=0,
   D=1,
   S=0,
-  L=[400000],
+  L=[],
   X=1,
   N=0;
   Object.defineProperty(globalThis.InternalError.prototype,"name",{
@@ -28,37 +28,36 @@
       if(X){
         if(_IF.state){
           I[++E]=[_IF.phase,_IF.cache,_IF.handler,_IF.args,_IF.delay+N,_IF.limit];
-          S++;
-          _IF.phase=400000;
-          _IF.cache=null
+          S++
         }
       }else{
         L[0]=_IF.phase;
-        _IF.phase=400000;
-        _IF.cache=null;
         _IF.wasInterrupted=!1;
         X=1
       }
+      _IF.cache=null;
       _IF.state=0;
       return"InternalError"
     }
   });
   _IF.tick=()=>{
+    _IF.state=0;
     if(!S){
+      _IF.cache=null;
       N++;
       return
     }
+    X=0;
+    _IF.wasInterrupted=!0;
     let l,
     m=E;
     while(D<=m){
       l=L=I[D];
       if(l[4]<=N){
         if(l[5]>0){
-          X=0;
           l[5]--;
           _IF.phase=l[0];
           _IF.cache=l[1];
-          _IF.wasInterrupted=!0;
           l[2](...l[3])
         }
         delete I[D++];
@@ -69,7 +68,6 @@
       }
     }
     _IF.state=0;
-    _IF.phase=400000;
     _IF.cache=null;
     _IF.wasInterrupted=!1;
     X=1;
--- src/interruption_framework_original.js
+++ src/interruption_framework_original.js
@@ -8,9 +8,9 @@
     handler: () => {},
     args: [],
     delay: 0,
-    limit: 2,
+    limit: 1,
 
-    phase: 1,
+    phase: 400000,
     cache: null,
 
     defaultPhase: 400000,
@@ -23,7 +23,7 @@
   let _enqueueId = 0;
   let _dequeueId = 1;
   let _queueSize = 0;
-  let _element = [400000];
+  let _element = [];
   let _external = 1;
   let _tickNum = 0;
 
@@ -34,26 +34,28 @@
         if (_IF.state) {
           _interrupted[++_enqueueId] = [_IF.phase, _IF.cache, _IF.handler, _IF.args, _IF.delay + _tickNum, _IF.limit];
           _queueSize++;
-          _IF.phase = 400000;
-          _IF.cache = null;
         }
       } else {
         _element[0] = _IF.phase;
-        _IF.phase = 400000;
-        _IF.cache = null;
         _IF.wasInterrupted = false;
         _external = 1;
       }
+      _IF.cache = null;
       _IF.state = 0;
       return "InternalError";
     },
   });
 
   _IF.tick = () => {
+    _IF.state = 0;
     if (!_queueSize) {
+      _IF.cache = null;
       _tickNum++;
       return;
     }
+
+    _external = 0;
+    _IF.wasInterrupted = true;
 
     let element;
     const maxDequeueId = _enqueueId;
@@ -61,11 +63,9 @@
       element = _element = _interrupted[_dequeueId];
       if (element[4] <= _tickNum) {
         if (element[5] > 0) {
-          _external = 0;
           element[5]--;
           _IF.phase = element[0];
           _IF.cache = element[1];
-          _IF.wasInterrupted = true;
           element[2](...element[3]);
         }
         delete _interrupted[_dequeueId++];
@@ -75,8 +75,8 @@
         _interrupted[++_enqueueId] = element;
       }
     }
+
     _IF.state = 0;
-    _IF.phase = 400000;
     _IF.cache = null;
     _IF.wasInterrupted = false;
     _external = 1;
